c04d8e608173d0106ff9b09f58ef2a5fd4282c27
Supphakit Duanghoy <46396282+fault2004@users.noreply.github.com>
2024-03-24 13:05:12 +0000

    stormtracking.sh - improved.
    remove 2 lines after processed data, view data using cat...

--- stormtracking.sh
+++ stormtracking.sh
@@ -7,8 +7,8 @@ STORMDATA="$HOME/stormdata"
 usage() {
 	echo "Usage: ./stormtracking.sh <mode> [options]"
 	echo "mode:"
-	echo " get [latest]     get data from TMD"
-	echo " view             view data"
+	echo " get [latest]     get data from TMD - latest, get only latest data"
+	echo " view [cat]       view data - use cat for reading"
 	echo " clear            clear data cache"
 	echo "This script using free RSS service from https://tmd.go.th/api/xml/storm-tracking"
 	exit 0
@@ -21,12 +21,19 @@ err() {
 
 viewdata() {
 	[ ! -f "$STORMDATA" ] && err 'data not found! please use "./stormtracking.sh get" first.'
-	more "$STORMDATA"
+	
+	if [ "$1" == "cat" ]; then
+		cat "$STORMDATA"
+	else
+		more "$STORMDATA"
+	fi
+	
 	exit 0
 }
 
 cleardata() {
 	[ -f "$STORMDATA" ] && rm "$STORMDATA"
+	[ -f "$STORMDATA.2" ] && rm "$STORMDATA.2"
 	exit 0
 }
 
@@ -35,7 +42,8 @@ getdata() {
 	DATA="$(curl -s "https://tmd.go.th/api/xml/storm-tracking")"
 	W_COUNT=$(echo "$DATA" | xmlstarlet sel -t -v "count(rss/channel/item)")
 	[ -f "$STORMDATA" ] && rm "$STORMDATA"
-	
+	[ -f "$STORMDATA.2" ] && rm "$STORMDATA.2"
+
 	[ "$1" == "latest" ] && W_COUNT="1"
 
 	echo "Please wait, processing data..."
@@ -43,10 +51,13 @@ getdata() {
 		W_TITLE=$(echo "$DATA" | xmlstarlet sel -t -v "(rss/channel/item[$i]/title)")
 		W_DEST=$(echo "$DATA" | xmlstarlet sel -t -v "(rss/channel/item[$i]/description)")
 
-		echo "$W_TITLE" >> "$STORMDATA"
-		echo "$W_DEST" >> "$STORMDATA"
-		echo >> "$STORMDATA"
+		echo "$W_TITLE" >> "$STORMDATA.2"
+		echo "$W_DEST" >> "$STORMDATA.2"
+		echo >> "$STORMDATA.2"
 	done
+
+	tail -n +4 "$STORMDATA.2" > "$STORMDATA"
+	rm "$STORMDATA.2"
 
 	echo "Saved in $STORMDATA"
 	echo 'type "./stormtracking.sh view" to view data'
@@ -61,7 +72,7 @@ case "$1" in
 		getdata "$2"
 	;;
 	"view")
-		viewdata
+		viewdata "$2"
 	;;
 	"clear")
 		cleardata
